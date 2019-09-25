8a1f8f91d36343624fd8d79b36ab0af471056328
RNolan19 <rnolan19@gmail.com>
2019-09-25 11:28:18 -0400

    added production apiURL to config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://tic-tac-toe-sei-production.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
